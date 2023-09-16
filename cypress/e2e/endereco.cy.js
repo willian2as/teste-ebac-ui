8f3af62acc8f60a35463d88ca9f674b97fe03647
Willian Almeida <aas.willian@gmail.com>
2023-09-16 19:32:38 -0300

    page objects e massa de dados

--- cypress/e2e/endereco.cy.js
+++ cypress/e2e/endereco.cy.js
@@ -1,4 +1,6 @@
 /// <reference types = "cypress" />
+import enderecoPage from "../support/page-objects/endereco.page";
+const dadosEndereco = require('../fixtures/endereco.json')
 
 describe('Funcionalidade Endereço - Faturamento e Entrega', () => {
     beforeEach(() => {
@@ -10,7 +12,26 @@ describe('Funcionalidade Endereço - Faturamento e Entrega', () => {
     });
     
     it('Deve fazer cadastro de faturamento com sucesso', () => {
-        
-        //cadastro de endereço
+        enderecoPage.editarEnderecoFaturamento('Jose', 'Maria', 'Petrobras', 'Brasil', 'Rua dos Bobos', '0', 'São Paulo', 'São Paulo', '86000-000', '+55 11 99999-9999', 'josemaria@email.com')
+
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
+    });
+
+    it('Deve fazer cadastro de faturamento com sucesso - Usando arquivo de dados', () => {
+        enderecoPage.editarEnderecoFaturamento
+            (dadosEndereco[2].nome,
+            dadosEndereco[2].sobrenome,
+            dadosEndereco[2].empresa,
+            dadosEndereco[2].pais,
+            dadosEndereco[2].endereco,
+            dadosEndereco[2].numero,
+            dadosEndereco[2].cidade,
+            dadosEndereco[2].estado,
+            dadosEndereco[2].cep,
+            dadosEndereco[2].telefone,
+            dadosEndereco[2].email
+            )
+
+        cy.get('.woocommerce-message').should('contain', 'Endereço alterado com sucesso.')
     });
 });
